14b02656289f0c108cc697be22202fefd5d7fa15
Venkataram <vesion4690@gmail.com>
2023-11-02 17:40:51 -0700

    Config based runner

--- run.py
+++ run.py
@@ -1,52 +1,56 @@
+import os
 import subprocess
-import argparse
+import sys
+import json
 
-cmds = [
-        'python combined.py meshes/nefertiti pos linear 250',
-        'python combined.py meshes/nefertiti onion linear 250',
-        
-        'python combined.py meshes/chinese pos linear 250',
-        'python combined.py meshes/chinese onion linear 250',
-        
-        'python combined.py meshes/lucy pos linear 250',
-        'python combined.py meshes/lucy onion linear 250',
-        
-        'python combined.py meshes/armadillo pos linear 250',
-        'python combined.py meshes/armadillo onion linear 250',
-]
+# TODO: multi GPU support
 
-for cmd in cmds:
-    print('  >', cmd)
-    subprocess.call(cmd.split())
+CONFIGS = os.path.join(os.path.dirname(__file__), 'configs')
+PROGRAM = os.path.join(os.path.dirname(__file__), 'combined.py')
+PYTHON = sys.executable
 
-# from configurations import *
-#
-# parser = argparse.ArgumentParser(description='Train models')
-# parser.add_argument('--name', type=str, default='nefertiti', help='name of the experiment')
-# parser.add_argument('--iterations', type=int, default=100_000, help='number of iterations')
-# parser.add_argument('--gpus', type=int, default=2, help='number of gpus')
-#
-# args = parser.parse_args()
-#
-# gpu_processes = {}
-#
-# next_gpu = 0
-# for m in models:
-#     for l in lerps:
-#         cmd = f'CUDA_VISIBLE_DEVICES={next_gpu} python train.py meshes/{args.name} {m} {l} {args.iterations}'
-#         gpu_processes.setdefault(next_gpu, []).append(cmd)
-#         next_gpu = (next_gpu + 1) % args.gpus
-#
-# # Fork for as many gpus as we have
-# import os
-#
-# for gpu, cmds in gpu_processes.items():
-#     pid = os.fork()
-#     if pid == 0:
-#         print(f'GPU {gpu} running {len(cmds)} commands')
-#         for cmd in cmds:
-#             print('  >', cmd)
-#             os.system(cmd)
-#         exit(0)
-#     else:
-#         print('Forked', pid)
+# Find all config files
+configs = []
+for f in os.listdir(CONFIGS):
+    if f.endswith('.json'):
+        configs.append(os.path.join(CONFIGS, f))
+
+# Process all configs
+commands = []
+
+for config in sorted(configs):
+    with open(config, 'r') as f:
+        data = json.load(f)
+
+    print('Detected config file', config)
+    confirm = input('  Run? [y/n] ')
+    if confirm.lower() != 'y':
+        continue
+
+    model = data['name']
+    target = data['target']
+    directory = data['directory']
+
+    print('  > Model name:       ', model)
+    print('  > Target:           ', target)
+    print('  > Results directory:', directory, end='\n\n')
+
+    # Build all commands to run
+    for experiment in data['experiments']:
+        name = experiment['name']
+        source = experiment['source']
+        method = experiment['method']
+        clusters = experiment['clusters']
+        batch = experiment['batch']
+        resolution = experiment['resolution']
+
+        print('  > Experiment:       ', name)
+        print('    > Source:         ', source)
+        print('    > Method:         ', method.split('/'))
+        print('    > Clusters:       ', clusters)
+        print('    > Batch size:     ', batch)
+        print('    > Resolution:     ', resolution)
+
+        cmd = f'{PYTHON} {PROGRAM} {model} {target} {source} {method} {clusters} {batch} {resolution} {directory}'
+        print('    > Command:        ', cmd)
+        commands.append(cmd)
