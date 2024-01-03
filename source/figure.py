337d4464222ac8bf340f66c7e34d0b8d8df71b26
Venkataram Sivaram <ves223@ucsd.edu>
2024-01-02 21:58:08 -0800

    Updating figures

--- source/figure.py
+++ source/figure.py
@@ -23,6 +23,8 @@ document_template = r'''
 \usepackage{amssymb}
 \usepackage{bm}
 
+\usetikzlibrary{calc}
+
 \definecolor{color0}{HTML}{ea7aa5}
 \definecolor{color1}{HTML}{65d59e}
 \definecolor{color2}{HTML}{ab92e6}
@@ -40,7 +42,9 @@ document_template = r'''
 
 \begin{document}
 \begin{center}
+\begin{tikzpicture}
 %s
+\end{tikzpicture}
 \end{center}
 \end{document}
 '''
@@ -64,15 +68,18 @@ combined_template = r'''
 \end{document}
 '''
 
-lineplot_template = r'''\begin{tikzpicture}
-\begin{axis}[
-    name=%s,%s
+lineplot_template = r'''\begin{axis}[
+    ymode=log,
+    title style={font=\small},
+    title=%s,
+    name=%s, %s
     xshift=2cm,
     width=%.2f cm,
     height=%.2f cm,
-    ylabel=\textsc{%s},
-    xlabel=\textsc{%s},
-    legend style={at={(0.5, 1.02)}, anchor=south},
+    ylabel=%s,
+    xlabel=%s,
+    y label style={at={(1.07, 0.5)}, rotate=180},
+    legend style={draw=none, fill=none, at={(0.5, -0.3)}, anchor=north, font=\footnotesize},
     legend columns=%d,
     xmin=%.3f,
     xmax=%.3f,
@@ -85,26 +92,25 @@ lineplot_template = r'''\begin{tikzpicture}
 
 %s
 
-\end{axis}
-\end{tikzpicture}'''
+\end{axis}'''
 
 def fill_lineplot(**kwargs):
-    return lineplot_template % (kwargs['codename'], kwargs['loc'],
-                                kwargs['width'], kwargs['height'],
-                                kwargs['ylabel'], kwargs['xlabel'],
-                                kwargs['num_cols'], kwargs['x_min'],
-                                kwargs['x_max'], kwargs['y_min'],
-                                kwargs['y_max'], kwargs['tick_step'],
-                                kwargs['plots'])
+    return lineplot_template % (kwargs['title'], kwargs['codename'],
+                                kwargs['loc'], kwargs['width'],
+                                kwargs['height'], kwargs['ylabel'],
+                                kwargs['xlabel'], 2,
+                                kwargs['x_min'], kwargs['x_max'],
+                                kwargs['y_min'], kwargs['y_max'],
+                                kwargs['tick_step'], kwargs['plots'])
 
 def plot_marked(key, d, color, legend):
     line = '\\addplot [mark=*, color=%s] coordinates {' % color
 
     for v in d:
-        line += '(%.4f, %.4f) ' % v
-    line += '};'
-
+        line += '(%f, %f) ' % v
+    line += '};\n'
     if legend:
+        line += '\\label{plot:' + key + '}'
         line += '\\addlegendentry{\\textsc{' + key + '}}'
 
     return line
@@ -114,18 +120,19 @@ def plot_transparent_end(key, d, color, legend):
 
     for v in d:
         line += '(%.4f, %.4f) ' % v
-    line += '};'
-
+    line += '};\n'
+    line += '\\label{' + key + '}'
     if legend:
-        line += '\\addlegendentry{\\textsc{' + key + '}}\n'
+        line += '\\addlegendentry{\\textsc{' + key.capitalize() + '}}\n'
 
     # Add coordinate at the end
-    line += '\\addplot [mark=*, color=%s, forget plot] coordinates { (%.4f, %.4f) };' % (color, d[-1][0], d[-1][1])
+    line += '\\addplot [mark=*, color=%s, forget plot] coordinates { (%f, %f) };' % (color, d[-1][0], d[-1][1])
 
     return line
 
-def lineplot(data, name, xlabel='X', ylabel='Y', width=8, height=6, mode='marked', at=None, legend=False):
+def lineplot(data, name, xlabel='X', ylabel='', width=8, height=6, mode='marked', at=None, legend=False):
     codename = name.lower().replace(' ', '-')
+    title = '\\textsc{%s}' % name.capitalize()
     print('Codename', codename, 'at', at)
 
     # Generate LaTeX code
@@ -140,7 +147,6 @@ def lineplot(data, name, xlabel='X', ylabel='Y', width=8, height=6, mode='marked
         else:
             raise NotImplementedError
 
-
     x_min = min([ min([ v[0] for v in d ]) for d in data.values() ])
     x_max = max([ max([ v[0] for v in d ]) for d in data.values() ])
     stride = (x_max - x_min) / 25
@@ -149,9 +155,13 @@ def lineplot(data, name, xlabel='X', ylabel='Y', width=8, height=6, mode='marked
 
     y_min = min([ min([ v[1] for v in d ]) for d in data.values() ])
     y_max = max([ max([ v[1] for v in d ]) for d in data.values() ])
+    print('yrange:', y_min, y_max)
+
     stride = (y_max - y_min) / 25
-    y_min = max(0, y_min - stride)
+    y_min -= stride
     y_max += stride
+
+    print('--> yrange:', y_min, y_max)
 
     # Find best tick step (5, 10, 25, etc.)
     tick_step = (x_max - x_min)
@@ -163,10 +173,11 @@ def lineplot(data, name, xlabel='X', ylabel='Y', width=8, height=6, mode='marked
     loc = '' if at is None else 'at={(%s)},' % at
     addplot = '\n'.join(lines)
 
-    tex = fill_lineplot(codename=codename, loc=loc, width=width, height=height,
-                        xlabel=xlabel, ylabel=ylabel, num_cols=len(data),
-                        x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max,
-                        tick_step=tick_step, plots=addplot)
+    tex = fill_lineplot(title=title, codename=codename, loc=loc, width=width,
+                        height=height, xlabel=xlabel, ylabel=ylabel,
+                        num_cols=len(data), x_min=x_min, x_max=x_max,
+                        y_min=y_min, y_max=y_max, tick_step=tick_step,
+                        plots=addplot)
 
     return codename, tex
 
@@ -182,7 +193,7 @@ def synthesize_tex(code, filename):
 
         os.makedirs(os.path.dirname('media/figures/generated/'), exist_ok=True)
         subprocess.check_call(['xelatex', '-interaction=nonstopmode', fp.name],
-            cwd=os.path.dirname('media/figures/generated/'), stdout=subprocess.DEVNULL)
+            cwd=os.path.dirname('media/figures/generated/')) #, stdout=subprocess.DEVNULL)
 
         # Copy resulting PDF to destination
         result = os.path.join('media/figures/generated/', os.path.basename(fp.name) + '.pdf')
@@ -361,9 +372,9 @@ def results_plot(name, db):
     print('Chamfer data', chamfer_data)
 
     # Generate lineplots
-    cn0, code0 = lineplot(render_data, 'Render Loss', ylabel='Loss', xlabel='Compression Ratio')
-    cn1, code1 = lineplot(normal_data, 'Normal Loss', at=cn0 + '.south east', ylabel='Loss', xlabel='Compression Ratio', legend=True)
-    _,   code2 = lineplot(chamfer_data, 'Chamfer Loss', at=cn1 + '.south east', ylabel='Loss', xlabel='Compression Ratio')
+    cn0, code0 = lineplot(render_data, 'Render Loss', ylabel='Error', xlabel='Compression Ratio', height=4)
+    cn1, code1 = lineplot(normal_data, 'Normal Loss', at=cn0 + '.south east', xlabel='Compression Ratio', height=4, legend=True)
+    _,   code2 = lineplot(chamfer_data, 'Chamfer Loss', at=cn1 + '.south east', xlabel='Compression Ratio', height=4)
 
     combined = code0 + '\n' + code1 + '\n' + code2
     combined = document_template % combined
@@ -393,7 +404,7 @@ def loss_plot(dir):
     lines = lambda f: open(os.path.join(dir, f), 'r').readlines()
     combine = lambda lines: ''.join(lines)
     csv = lambda f: combine(lines(f)).split(',')
-    pointed = lambda f: [ (i, float(l)) for i, l in enumerate(csv(f)) ]
+    pointed = lambda f: [ (i, np.log10(float(l))) for i, l in enumerate(csv(f)) ]
 
     graphs = { strip(f) : pointed(f) for f in files }
 
@@ -402,6 +413,278 @@ def loss_plot(dir):
 
     tex = document_template % tex
     synthesize_tex(tex, 'losses.pdf')
+
+# Tabl generation
+def table(dbs):
+    from prettytable import PrettyTable
+
+    def round_nines(x, k):
+        if k == 'Ours':
+            return int(np.ceil(x/10) * 10)
+        else:
+            return int(np.ceil(x/1000) * 1000)
+
+    converted = {}
+    for scene, db in dbs.items():
+        print('Scene', scene)
+        for k, ddb in db.items():
+            print('  >', k)
+            if k == 'reference':
+                converted.setdefault('size', {})
+                converted['size'][scene] = ddb
+            else:
+                # converted.setdefault(k, {})
+                for kk in ddb:
+                    count = round_nines(kk['count'], k)
+                    print('     >', kk, count)
+                    t = (k, count)
+                    # converted[k].setdefault(count, {})
+                    converted.setdefault(t, {})
+                    converted[t][scene] = kk
+
+    # print(converted)
+    for k, db in converted.items():
+        print(k, '->', db)
+
+    tbl = PrettyTable()
+
+    # tbl.field_names = [] # [ 'Primitives', 'Size' ]
+
+    field_names = [ '', 'Primitives', 'Size (KB)' ]
+    for scene in dbs:
+        field_names.append(scene)
+
+    tbl.field_names = field_names
+
+    # Sizes
+    sizes = []
+    for f in field_names:
+        if f == '':
+            sizes.append('Mesh size (MB/Triangles)')
+            continue
+
+        if f in [ 'Primitives', 'Size (KB)' ]:
+            sizes.append('')
+            continue
+
+        sizem = converted['size'][f]
+        sizemb = sizem['size'] / 1024 ** 2
+        sizetris = sizem['count']
+
+        sizes.append('%.2f MB / %dK' % (sizemb, sizetris / 1000))
+
+    tbl.add_row(sizes, divider=True)
+
+    # Each method
+    to_add = []
+    for t in converted:
+        if not t[0] == 'Ours':
+            continue
+
+        data = converted[t]
+
+        sizem = [ d['size'] for d in list(data.values()) ]
+        sizem = int(sum(sizem)/len(sizem))
+
+        row = []
+        for f in field_names:
+            if f == '':
+                row.append('Ours')
+                continue
+            if f == 'Primitives':
+                row.append(t[1])
+                continue
+            if f == 'Size (KB)':
+                row.append(sizem // 1024)
+                continue
+
+            if not f in data:
+                row.append('')
+                continue
+
+            d = data[f]
+            row.append('%.2f / %.2f / %.2f' % (1e2 * d['render'], 1e2 * d['normal'], 1e5 * d['chamfer']))
+
+        to_add.append(row)
+
+    for i in range(len(to_add)):
+        if i == len(to_add) - 1:
+            tbl.add_row(to_add[i], divider=True)
+        else:
+            tbl.add_row(to_add[i])
+
+    to_add = []
+    for t in converted:
+        if not t[0] == 'QSlim':
+            continue
+
+        data = converted[t]
+
+        sizem = [ d['size'] for d in list(data.values()) ]
+        sizem = max(sizem)
+
+        row = []
+        for f in field_names:
+            if f == '':
+                row.append('QSlim')
+                continue
+            if f == 'Primitives':
+                row.append(t[1])
+                continue
+            if f == 'Size (KB)':
+                row.append(sizem // 1024)
+                continue
+
+            if not f in data:
+                row.append('')
+                continue
+
+            d = data[f]
+            row.append('%.2f / %.2f / %.2f' % (1e2 * d['render'], 1e2 * d['normal'], 1e5 * d['chamfer']))
+
+        to_add.append(row)
+
+    for i in range(len(to_add)):
+        if i == len(to_add) - 1:
+            tbl.add_row(to_add[i], divider=True)
+        else:
+            tbl.add_row(to_add[i])
+
+    for t in converted:
+        if not t[0] == 'nvdiffmodeling':
+            continue
+
+        data = converted[t]
+
+        sizem = [ d['size'] for d in list(data.values()) ]
+        sizem = max(sizem)
+
+        row = []
+        for f in field_names:
+            if f == '':
+                row.append('nvdiffmodeling')
+                continue
+            if f == 'Primitives':
+                row.append(t[1])
+                continue
+            if f == 'Size (KB)':
+                row.append(sizem // 1024)
+                continue
+
+            if not f in data:
+                row.append('')
+                continue
+
+            d = data[f]
+            row.append('%.2f / %.2f / %.2f' % (1e2 * d['render'], 1e2 * d['normal'], 1e5 * d['chamfer']))
+
+        # print(row)
+        tbl.add_row(row)
+
+    print(tbl.get_latex_string())
+
+def tessellation(db):
+    print(db)
+
+    render = {}
+    normal = {}
+    chamfer = {}
+
+    for scene in db:
+        render_line = []
+        normal_line = []
+        chamfer_line = []
+
+        for entry in db[scene]:
+            render_error = db[scene][entry]['render']
+            normal_error = db[scene][entry]['normal']
+            chamfer_error = db[scene][entry]['chamfer']
+
+            entry = int(entry)
+            # render_line.append((entry, np.log10(render_error)))
+            # normal_line.append((entry, np.log10(normal_error)))
+            # chamfer_line.append((entry, np.log10(chamfer_error)))
+
+            render_line.append((entry, render_error))
+            normal_line.append((entry, normal_error))
+            chamfer_line.append((entry, chamfer_error))
+
+        render[scene] = render_line
+        normal[scene] = normal_line
+        chamfer[scene] = chamfer_line
+
+    n0, render_code = lineplot(render, 'Render', xlabel='Tessellation', width=8)
+    n1, normal_code = lineplot(normal, 'Normal', xlabel='Tessellation', at=n0 + '.south east', width=8, legend=True)
+    n2, chamfer_code = lineplot(chamfer, 'Chamfer', xlabel='Tessellation', ylabel='Error', at=n1 + '.south east', width=8)
+
+    combined = render_code + '\n' + normal_code + '\n' + chamfer_code
+    combined = document_template % combined
+
+    os.makedirs('tex', exist_ok=True)
+    with open('tex/tessellation.tex', 'w') as f:
+        f.write(combined)
+
+    synthesize_tex(combined, 'tessellation.pdf')
+
+def features(db):
+    print(db)
+
+    render = {}
+    normal = {}
+    chamfer = {}
+
+    for scene in db:
+        render_line = []
+        normal_line = []
+        chamfer_line = []
+
+        entries = db[scene]
+        entries = list(entries.items())
+        entries = [ (int(f), d) for f, d in entries ]
+        entries = sorted(entries, key=lambda v: v[0])
+        print('entries', entries)
+        if len(entries) < 4:
+            continue
+
+        for f, data in entries:
+            render_error = data['render']
+            normal_error = data['normal']
+            chamfer_error = data['chamfer']
+            size = data['size'] // 1024
+
+            render_line.append((size, render_error))
+            normal_line.append((size, normal_error))
+            chamfer_line.append((size, chamfer_error))
+
+        render[scene] = render_line
+        normal[scene] = normal_line
+        chamfer[scene] = chamfer_line
+
+    n0, render_code = lineplot(render, 'Render', xlabel='Size (KB)')
+    n1, normal_code = lineplot(normal, 'Normal', xlabel='Size (KB)', at=n0 + '.south east', legend=True)
+    _,  chamfer_code = lineplot(chamfer, 'Chamfer', xlabel='Size (KB)', ylabel='Error', at=n1 + '.south east')
+
+    combined = render_code + '\n' + normal_code + '\n' + chamfer_code
+    combined = document_template % combined
+
+    synthesize_tex(combined, 'features.pdf')
+
+def gimgs(db):
+    measure = {}
+    for k in db:
+        mean = db[k]['mean']
+        size = db[k]['size'] // 1024
+        gsize = db[k]['gsize'] // 1024
+
+        measure.setdefault('NGF (Ours)', []).append((size, mean))
+        measure.setdefault('Geometry Image', []).append((gsize, mean))
+
+    print(measure)
+
+    _, code = lineplot(measure, '', ylabel='Error', xlabel='Size (KB)', width=8, legend=True)
+
+    combined = document_template % code
+    synthesize_tex(combined, 'geometry-images.pdf')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -421,5 +704,27 @@ if __name__ == '__main__':
                 results_plot(key, db[key])
         else:
             results_plot(args.key, db[args.key])
+    elif args.type == 'table':
+        dbs = {}
+        for root, directory, files in os.walk(args.dir):
+            if not root == os.path.basename(args.dir):
+                continue
+
+            for file in files:
+                if file.endswith('.json'):
+                    db = json.load(open(os.path.join(root, file)))
+                    f = file.split('.')[0].capitalize()
+                    dbs[f] = db
+
+        table(dbs)
+    elif args.type == 'tess':
+        db = json.load(open(args.db))
+        tessellation(db)
+    elif args.type == 'features':
+        db = json.load(open(args.db))
+        features(db)
+    elif args.type == 'gimgs':
+        db = json.load(open(args.db))
+        gimgs(db)
     else:
         raise NotImplementedError
