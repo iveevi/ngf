7f6e7beefaae55d3e3664adb792ac91429c5c73a
Venkataram Edavamadathil Sivaram <vesion4690@gmail.com>
2024-01-21 18:19:06 -0800

    Additional model configs

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 set -v
-python3 source/train.py configs/lods/nefertiti.json
-python3 source/train.py configs/lods/xyz.json
-python3 source/train.py configs/lods/lucy.json
-python3 source/train.py configs/lods/einstein.json
 python3 source/train.py configs/lods/armadillo.json
+python3 source/train.py configs/lods/dragon.json
+python3 source/train.py configs/lods/einstein.json
+python3 source/train.py configs/lods/igea.json
+python3 source/train.py configs/lods/lucy.json
 python3 source/train.py configs/lods/lucy.json
 python3 source/train.py configs/lods/metatron.json
-python3 source/train.py configs/lods/venus.json
+python3 source/train.py configs/lods/nefertiti.json
 python3 source/train.py configs/lods/skull.json
-python3 source/train.py configs/lods/dragon.json
+python3 source/train.py configs/lods/venus.json
+python3 source/train.py configs/lods/xyz.json
