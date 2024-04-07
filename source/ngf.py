Fixing texture fetch gradients

source/ngf.py
@@ -8,6 +8,8 @@ import torch.nn as nn
 
 from typing import Callable
 
+from util import SIREN
+
 
 # TODO: try a SIREN network...
 class MLP(nn.Module):
@@ -44,13 +46,8 @@ class MLP(nn.Module):
         return bytestream
 
 
-class Sampler:
-    def __init__(self, points: torch.Tensor, features: torch.Tensor, complexes: torch.Tensor):
-        pass
-
-
 def spherical_to_cartesian(spherical):
-    radius = spherical[..., 0].square()
+    radius = spherical[..., 0]
     theta = spherical[..., 1]
     phi = spherical[..., 2]
     x = theta.cos() * phi.sin()
@@ -59,7 +56,13 @@ def spherical_to_cartesian(spherical):
     return radius.unsqueeze(-1) * torch.stack((x, y, z), dim=-1)
 
 
+@torch.jit.script
+def triangle_wave(x):
+    return (2 * x.fmod(1) - 1).abs()
+
+
 # Positional encoding
+@torch.jit.script
 def positional_encoding(vector: torch.Tensor, levels: int) -> list[torch.Tensor]:
     result = []
     for i in range(levels):
@@ -84,7 +87,8 @@ class NGF:
         self.jittering = jittering
         self.normals = normals
 
-        self.ffin = self.features.shape[-1] + 3 * (2 * self.fflevels)
+        # self.ffin = self.features.shape[-1] + 3 * (2 * self.fflevels)
+        self.ffin = self.features.shape[-1] + 2 * 24
         self.mlp = MLP(self.ffin).cuda()
         if mlp is not None:
             self.mlp.load_state_dict(mlp.state_dict())
@@ -105,6 +109,9 @@ class NGF:
         logging.info(f'     FF levels:    {fflevels}')
         logging.info(f'     Jittering:    {jittering}')
         logging.info(f'     Normals:      {normals}')
+
+        std = 32 * torch.ones((24, 3), device='cuda')
+        self.rff = torch.normal(torch.zeros_like(std), std)
 
     # List of parameters
     def parameters(self):
@@ -134,8 +141,9 @@ class NGF:
 
         lf = (lf00 + lf01 + lf10 + lf11).reshape(-1, fsize)
 
-        lin = positional_encoding(lp, self.fflevels) + [lf]
-        lin = torch.cat(lin, dim=-1)
+        lin = lp @ self.rff.T
+        # lin = positional_encoding(lp, self.fflevels) + [lf]
+        lin = torch.cat((lin.sin(), lin.cos(), lf), dim=-1)
         return lp + self.mlp(lin)
 
     def base(self, rate):
@@ -184,10 +192,15 @@ class NGF:
         delta = 0.45/(rate - 1)
 
         # TODO: use a normal distribution with decreasing jittering...
-        rand_uv = (2 * torch.rand((2, U.shape[0], U.shape[1]), device='cuda') - 1)
-        rand_uv *= delta * UV_interior
+        # rand_uv = (2 * torch.rand((2, U.shape[0], U.shape[1]), device='cuda') - 1)
+        # rand_uv *= delta * UV_interior
+        rtheta = 2 * np.pi * torch.rand(*U.shape, device='cuda')
+        rr = torch.rand(*U.shape, device='cuda').sqrt()
+        ru = delta * rr * rtheta.cos() * UV_interior
+        rv = delta * rr * rtheta.sin() * UV_interior
 
-        return U + rand_uv[0], V + rand_uv[1]
+        # return U + rand_uv[0], V + rand_uv[1]
+        return U + ru, V + rv
 
     def save(self, filename):
         """Save into a PyTorch (PT) file"""
@@ -224,8 +237,8 @@ class NGF:
         complexes = torch.from_numpy(mesh.cells_dict['quad'])
 
         points = normalizer(points.float().cuda())
-        features = torch.randn((points.shape[0], features)).cuda()
-        # features = torch.zeros((points.shape[0], features)).cuda()
+        # features = torch.randn((points.shape[0], features)).cuda()
+        features = torch.zeros((points.shape[0], features)).cuda()
         complexes = complexes.int().cuda()
 
         points.requires_grad = True

source/render.py
@@ -1,53 +1,45 @@
 import os
 import torch
 import numpy as np
-import imageio
 import nvdiffrast.torch as dr
 
 
-def persp_proj(fov_x, ar, near, far):
-    fov_rad = np.deg2rad(fov_x)
-    proj_mat = np.array([
-        [-1.0 / np.tan(fov_rad / 2.0), 0, 0, 0],
-        [0, np.float32(ar) / np.tan(fov_rad / 2.0), 0, 0],
-        [0, 0, -(near + far) / (near-far), 2 * far * near / (near-far)],
-        [0, 0, 1, 0]
-    ])
-
-    return torch.tensor(proj_mat, device='cuda', dtype=torch.float32)
-
-
-# NOTE: Adapted from the 'Large Steps in Inverse Rendering' codebase
 class Renderer:
     ENVIRONMENT = os.path.join(os.path.dirname(__file__), os.path.pardir, 'media', 'environment.hdr')
+
+    @staticmethod
+    def projection(fov: float, ar: float, near: float, far: float) -> torch.Tensor:
+        fov_rad = np.deg2rad(fov)
+        proj_mat = np.array([
+            [-1.0 / np.tan(fov_rad / 2.0), 0, 0, 0],
+            [0, np.float32(ar) / np.tan(fov_rad / 2.0), 0, 0],
+            [0, 0, -(near + far) / (near-far), 2 * far * near / (near-far)],
+            [0, 0, 1, 0]
+        ])
+
+        return torch.tensor(proj_mat, device='cuda', dtype=torch.float32)
 
     def __init__(self,
                  width: int = 256,
                  height: int = 256,
                  fov: float = 45.0,
                  near:float = 0.1,
-                 far: float = 1000.0,
-                 environment: str = ENVIRONMENT) -> None:
+                 far: float = 1000.0) -> None:
         self.res = (height, width)
-        self.proj = persp_proj(fov, width/height, near, far)
+        self.proj = Renderer.projection(fov, width/height, near, far)
         self.ctx = dr.RasterizeCudaContext()
 
-    def render(self, v, n, f, views):
+    def render(self, v: torch.Tensor, n: torch.Tensor, f: torch.Tensor, views: torch.Tensor) -> torch.Tensor:
         mvps = self.proj @ views
         v_hom = torch.nn.functional.pad(v, (0, 1), 'constant', 1.0)
         v_ndc = torch.matmul(v_hom, mvps.transpose(1, 2))
 
         layers = []
         with dr.DepthPeeler(self.ctx, v_ndc, f, self.res) as peeler:
-            for i in range(2):
+            for i in range(3):
                 rast, rast_db = peeler.rasterize_next_layer()
-
-                vertices = dr.interpolate(v, rast, f)[0]
-                vertices = dr.antialias(vertices, rast, v_ndc, f)
-
                 normals = dr.interpolate(n, rast, f)[0]
                 normals = dr.antialias(normals, rast, v_ndc, f)
-
-                layers += [vertices, normals]
+                layers += [normals]
 
         return torch.concat(layers, dim=-1)

source/train.py
@@ -18,7 +18,7 @@ class Trainer:
         # Properties
         self.path = os.path.abspath(mesh)
         self.cameras = 200
-        self.batch = 25
+        self.batch = 20
         self.losses = {}
 
         logging.info('Launching training process with configuration:')
@@ -54,17 +54,24 @@ class Trainer:
         faces = faces.int().cuda().reshape(-1, 3)
         normals = vertex_normals(vertices, faces)
 
-        cache = []
-        for batch_views in self.views.split(self.batch):
-            reference_views = self.renderer.render(vertices, normals, faces, batch_views)
-            # fig, axs = plt.subplots(5, 5, layout='constrained')
-            # for ax, img in zip(axs.flatten(), reference_views.cpu()):
-            #     ax.imshow(0.5 + 0.5 * img[..., 3:6])
-            #     ax.axis('off')
-            # plt.show()
-            cache.append(reference_views.cpu())
+        # vertices = self.target.vertices
+        # faces = self.target.faces
+        # colors = torch.rand_like(normals)
 
-        return cache
+        cache = []
+        # for batch_views in self.views.split(self.batch):
+        #     reference_views = self.renderer.render(vertices, normals, faces, batch_views)
+        #     # fig, axs = plt.subplots(5, 5, layout='constrained')
+        #     # for ax, img in zip(axs.flatten(), reference_views.cpu()):
+        #     #     ax.imshow(img)
+        #     #     ax.axis('off')
+        #     # plt.show()
+        #     cache.append(reference_views.cpu())
+        for view in tqdm.tqdm(self.views, ncols=50, leave=False):
+            reference_view = self.renderer.render(vertices, normals, faces, view.unsqueeze(0))
+            cache.append(reference_view)
+
+        return torch.cat(cache).split(self.batch)
 
     def initialize_ngf(self) -> None:
         with torch.no_grad():
@@ -84,8 +91,6 @@ class Trainer:
         logging.info('Neural geometry field initialization phase done')
 
     def optimize_resolution(self, optimizer: torch.optim.Optimizer, rate: int) -> dict[str, list[float]]:
-        import torch.autograd.forward_ad as fwad
-
         losses = {
             'render': [],
             'laplacian': []
@@ -107,7 +112,7 @@ class Trainer:
             uvs = self.ngf.sampler(rate)
             uniform_uvs = self.ngf.sample_uniform(rate)
 
-            # TODO: shuffle the views as well
+            # # TODO: shuffle the views as well
             for batch_reference_views, batch_views in zip(self.reference_views, batched_views):
                 vertices = self.ngf.eval(*uvs)
                 uniform_vertices = self.ngf.eval(*uniform_uvs)
@@ -122,7 +127,7 @@ class Trainer:
                 batch_source_views = self.renderer.render(vertices, normals, faces, batch_views)
 
                 render_loss = (batch_reference_views.cuda() - batch_source_views).abs().mean()
-                loss = render_loss
+                loss = render_loss + laplacian_loss
 
                 optimizer.zero_grad()
                 loss.backward()
@@ -146,15 +151,14 @@ class Trainer:
             'laplacian': []
         }
 
-        opt = torch.optim.Adam(self.ngf.parameters(), 1e-3)
-
         self.views = arrange_views(self.target, self.cameras)[0]
         logging.info(f'Generated {self.cameras} views for reference mesh')
 
         self.reference_views = self.precompute_reference_views()
         logging.info('Cached reference views')
 
-        for rate in [2, 4, 8]:
+        opt = torch.optim.Adam(self.ngf.parameters(), 1e-3)
+        for rate in [4, 8, 16]:
             rate_losses = self.optimize_resolution(opt, rate)
             self.losses['render'] += rate_losses['render']
             self.losses['laplacian'] += rate_losses['laplacian']
@@ -249,4 +253,4 @@ if __name__ == '__main__':
     trainer.export()
 
     if args.display:
-        trainer.display(8)
+        trainer.display()

source/util/miscellaneous.py
@@ -1,4 +1,5 @@
 import torch
+import ngfutil
 import numpy as np
 
 from .mesh import Mesh
@@ -148,13 +149,12 @@ def lookat(eye, center, up):
 
 
 def arrange_views(simplified: Mesh, cameras: int, radius: float = 1.0):
-    import ngfutil
-
     seeds = list(torch.randint(0, simplified.faces.shape[0], (cameras,)).numpy())
-    clusters = ngfutil.cluster_geometry(simplified.optg, seeds, 10, 'uniform')
+    clusters = ngfutil.cluster_geometry(simplified.optg, seeds, 3, 'uniform')
 
     views = []
     eyes = []
+    fwds = []
     for cluster in clusters:
         faces = simplified.faces[cluster]
 
@@ -194,5 +194,14 @@ def arrange_views(simplified: Mesh, cameras: int, radius: float = 1.0):
 
         views.append(view)
         eyes.append(eye)
+        fwds.append(look)
+
+    # import polyscope as ps
+    # ps.init()
+    # v = torch.stack(eyes)
+    # f = torch.stack(fwds)
+    # ps.register_surface_mesh('ref', simplified.vertices.cpu().numpy(), simplified.faces.cpu().numpy())
+    # ps.register_point_cloud('eyes', v.cpu()).add_vector_quantity('looks', f.cpu())
+    # ps.show()
 
     return torch.stack(views), torch.stack(eyes)